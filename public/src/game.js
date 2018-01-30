efb5ae23a8b4face4e3584425cd6696ecd759db1
Delveri Chick <chickdelveri@gmail.com>
2018-01-30 18:51:56 -0500

    Added Scene Transitions

--- public/src/game.js
+++ public/src/game.js
@@ -1,1 +1,57 @@
-alert("Game Started");
+var splashScreen = document.getElementById('splash');
+var game = document.getElementById('main_game');
+var game_over = document.getElementById('game_over');
+
+
+function hideScene(mode){
+  mode.style.display = 'none';
+}
+
+function showScene(scene){
+  scene.style.display = 'block';
+}
+
+function setScene(scene){
+
+  var ctx = scene.getContext('2d');
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  ctx.beginPath();
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+
+window.onload = function(){
+    hideScene(game);
+    hideScene(game_over);
+    setScene(splashScreen);
+    initScene(splashScreen);
+}
+
+function initScene(scene){
+  scene.addEventListener('click', function(){
+      alert('GAME SCREEN');
+      hideScene(scene);
+      showScene(game);
+      StartGame(game);
+
+  });
+}
+
+function StartGame(scene){
+  scene.addEventListener('click', function(){
+      alert('GAME OVER SCREEN');
+      endGame();
+  });
+  var ctx = scene.getContext('2d');
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  ctx.fillRect(20, 20, 150, 100);
+}
+
+function endGame(){
+  hideScene(game);
+  hideScene(splashScreen);
+  showScene(game_over);
+}
