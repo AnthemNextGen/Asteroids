15c0fdb6f1882f49bf62acf600c91ba796239328
delveri <delveri.chick@anthem.com>
2018-01-31 16:10:36 -0500

    Added Util functions

--- public/src/game.js
+++ public/src/game.js
@@ -9,20 +9,31 @@ function hideScene(mode){
 
 function showScene(scene){
   scene.style.display = 'block';
+
 }
 
 function setScene(scene){
-
   var ctx = scene.getContext('2d');
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
   ctx.beginPath();
-  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-  ctx.fillStyle = 'red';
+  ctx.font = "30px Arial";
+  ctx.fillStyle = 'teal';
+  ctx.fillText("ASTEROID NEXTGEN",innerWidth/3,innerHeight/5);
+  ctx.fillRect(innerWidth/6,300, 200,75);
+  ctx.fillRect(850,300, 200,75);
   ctx.fill();
   ctx.stroke();
+  generateCircles(ctx);
 }
 
+function generateCircles(ctx){
+    var posX = Math.random()*innerWidth;
+    var posY = Math.random()*innerHeight;
+    ctx.arc(posX++,posY++,30,0, Math.PI*2, false);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+}
 
 window.onload = function(){
     hideScene(game);
@@ -58,6 +69,7 @@ function StartGame(scene){
   ctx.canvas.height = window.innerHeight;
   ctx.fillStyle = 'aqua';
   ctx.fillRect(20, 20, 150, 100);
+  generateCircles(ctx);
 }
 
 function endGame(){
