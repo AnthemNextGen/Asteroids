48071f13f460c78da54f1898fcc54ec91b680970
unknown <AF66400@LG1R4QH2.us.ad.wellpoint.com>
2018-02-01 09:57:48 -0500

    Music file added

--- public/src/game.js
+++ public/src/game.js
@@ -1,6 +1,7 @@
 var splashScreen = document.getElementById('splash');
 var game = document.getElementById('main_game');
 var game_over = document.getElementById('game_over');
+var music = document.getElementById('music');
 
 
 function hideScene(mode){
@@ -25,7 +26,8 @@ function setScene(scene){
 
 
 window.onload = function(){
-    hideScene(game);
+    music.play();
+    hideScene(game)
     hideScene(game_over);
     setScene(splashScreen);
     initScene(splashScreen);
@@ -34,19 +36,22 @@ window.onload = function(){
 function initScene(scene){
   scene.addEventListener('click', function(){
       alert('GAME SCREEN');
+      music.pause();
       hideScene(scene);
       showScene(game);
-      StartGame(game);
+      StartGame(game, music.pause());
 
   });
 }
 
 function StartGame(scene){
   var socket = io();
+  music.play();
   var user = prompt('Enter Player Name');
   socket.emit('join', user);
   socket.on('join', function(user){
     alert(user + "  Connected");
+    music.pause();
   });
   scene.addEventListener('click', function(){
       alert('GAME OVER SCREEN');
@@ -57,11 +62,13 @@ function StartGame(scene){
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
   ctx.fillStyle = 'aqua';
-  ctx.fillRect(20, 20, 150, 100);
+  ctx.fillRect(150, 0, 640, 680);
 }
 
 function endGame(){
+  music.play();
   hideScene(game);
   hideScene(splashScreen);
   showScene(game_over);
 }
+
