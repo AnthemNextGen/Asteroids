eed56f97c86fee6bd7b3ba0caee56d65bdb410af
unknown <AF66400@LG1R4QH2.us.ad.wellpoint.com>
2018-02-01 10:45:20 -0500

    Formatted code for Push- loop added for music replay

--- public/src/game.js
+++ public/src/game.js
@@ -4,69 +4,69 @@ var game_over = document.getElementById('game_over');
 var music = document.getElementById('music');
 
 
-function hideScene(mode){
+function hideScene(mode) {
   mode.style.display = 'none';
 }
 
-function showScene(scene){
+function showScene(scene) {
   scene.style.display = 'block';
 
 }
 
-function setScene(scene){
+function setScene(scene) {
   var ctx = scene.getContext('2d');
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
   ctx.beginPath();
   ctx.font = "30px Arial";
   ctx.fillStyle = 'teal';
-  ctx.fillText("ASTEROID NEXTGEN",innerWidth/3,innerHeight/5);
-  ctx.fillRect(innerWidth/6,300, 200,75);
-  ctx.fillRect(850,300, 200,75);
+  ctx.fillText("ASTEROID NEXTGEN", innerWidth / 3, innerHeight / 5);
+  ctx.fillRect(innerWidth / 6, 300, 200, 75);
+  ctx.fillRect(850, 300, 200, 75);
   ctx.fill();
   ctx.stroke();
   generateCircles(ctx);
 }
 
-function generateCircles(ctx){
-    var posX = Math.random()*innerWidth;
-    var posY = Math.random()*innerHeight;
-    ctx.arc(posX++,posY++,30,0, Math.PI*2, false);
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
+function generateCircles(ctx) {
+  var posX = Math.random() * innerWidth;
+  var posY = Math.random() * innerHeight;
+  ctx.arc(posX++, posY++, 30, 0, Math.PI * 2, false);
+  ctx.strokeStyle = 'red';
+  ctx.stroke();
 }
 
-window.onload = function(){
-    music.play();
-    hideScene(game)
-    hideScene(game_over);
-    setScene(splashScreen);
-    initScene(splashScreen);
+window.onload = function () {
+  music.play();
+  hideScene(game)
+  hideScene(game_over);
+  setScene(splashScreen);
+  initScene(splashScreen);
 }
 
-function initScene(scene){
-  scene.addEventListener('click', function(){
-      alert('GAME SCREEN');
-      music.pause();
-      hideScene(scene);
-      showScene(game);
-      StartGame(game);
+function initScene(scene) {
+  scene.addEventListener('click', function () {
+    alert('GAME SCREEN');
+    music.pause();
+    hideScene(scene);
+    showScene(game);
+    StartGame(game);
 
   });
 }
 
-function StartGame(scene){
+function StartGame(scene) {
   var socket = io();
   music.play();
   var user = prompt('Enter Player Name');
   socket.emit('join', user);
-  socket.on('join', function(user){
+  socket.on('join', function (user) {
     alert(user + "  Connected");
     music.pause();
   });
-  scene.addEventListener('click', function(){
-      alert('GAME OVER SCREEN');
-      endGame();
+  scene.addEventListener('click', function () {
+    alert('GAME OVER SCREEN');
+    endGame();
 
   });
   var ctx = scene.getContext('2d');
@@ -77,7 +77,7 @@ function StartGame(scene){
   generateCircles(ctx);
 }
 
-function endGame(){
+function endGame() {
   music.play();
   hideScene(game);
   hideScene(splashScreen);
