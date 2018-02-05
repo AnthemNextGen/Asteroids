cfd00a6186890d6643e37a361f94b0c40c4bad82
unknown <AF66400@LG1R4QH2.us.ad.wellpoint.com>
2018-02-05 14:14:35 -0500

    Added unit test for music functions

--- public/spec/FunctionSpec.js
+++ public/spec/FunctionSpec.js
@@ -1,9 +1,19 @@
-describe('Should test all', function(){
-  it('Should always pass', function(){
-      expect(true).toBe(true);
+describe('Should test all', function () {
+  it('Should always pass', function () {
+    expect(true).toBe(true);
   });
 
-  it('Draw shape on screen', function(){
+  it('Draw shape on screen', function () {
     expect(drawShape()).toBe(true);
   });
+
+  it('Play Music', function () {
+    expect(playMusic(url)).toBeTruthy();
+  });
+
+  it('Pause Music', function () {
+    expect(pauseMusic(url)).toBeTruthy();
+  });
+
 });
+
